3b27d99c526c4d7826652c45562594cf670b4346
Alma Maria <alma_maria@ucne.edu.do>
2025-12-01 15:16:28 -0400

    refactor ♻️: elimina entidades

--- app/src/main/java/edu/ucne/tasktally/domain/models/Persona.kt
+++
@@ -1,8 +0,0 @@
-package edu.ucne.tasktally.domain.models
-
-data class Persona(
-    val personaId: Int = 0,
-    val userId: Int = 0,
-    val nombre: String,
-    val rewardTotal: Double = 0.0
-)
--- app/src/main/java/edu/ucne/tasktally/domain/models/PersonaRecompensa.kt
+++
@@ -1,8 +0,0 @@
-package edu.ucne.tasktally.domain.models
-
-data class PersonaRecompensa(
-    val personaRecompensaId: Int = 0,
-    val personaId: Int = 0,
-    val recompensaId: Int = 0,
-    val fechaCanje: String
-)
